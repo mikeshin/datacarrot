35c84fc630dc939f2c2dc91448088281b04d13a7
Mike Shin <mikeshin@gmail.com>
2014-01-03 17:47:36 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Datacarrot::Application.config.secret_key_base = '07762f39a26d4031665020c2a2787ecbf6d286cc926bb572ff3b1d1dfed1582f37d8cd2e01d1f7e8f71726fe0eaa781d2576d82c9c6cf5d608f88d3fcd6eb426'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Datacarrot::Application.config.session_store :cookie_store, key: '_datacarrot_session'
